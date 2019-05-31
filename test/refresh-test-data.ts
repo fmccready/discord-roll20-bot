3c683e28f1fdca8ae43c77681c2b4bf45211f2ca
Frank McCready <fmccready@gmail.com>
2019-05-30 22:46:59 -0500

    Write example data into markdown files

--- test/refresh-test-data.ts
+++ test/refresh-test-data.ts
@@ -1,66 +1,62 @@
 import chalk from 'chalk'
-import { Client } from 'discord.js'
+import { Channel, Client, Guild, TextChannel } from 'discord.js'
 import * as dotenv from 'dotenv'
 import { createWriteStream, mkdir } from 'fs'
 import { WriteStream } from 'fs'
 import * as path from 'path'
+import { forkJoin, from, Observable, of } from 'rxjs'
 import { inspect } from 'util'
 
 dotenv.config()
 
-interface TestDataWriter {
-  data: () => object | Iterable<any>
+interface Test {
+  data: () => Observable<object | IterableIterator<Guild | Channel> | Client>
   file: string
-  writer: (writeStream: WriteStream, data: any) => void
 }
 
 const client = new Client()
 
-const testDataFiles: TestDataWriter[] = [
+const testDataFiles: Test[] = [
   {
-    data: () => client.channels.values(),
-    file: 'channels.json',
-    writer: writeIterableToWriteStream,
+    data: () => from(client.channels.values()),
+    file: 'channels.md',
   },
   {
-    data: () => client.guilds.values(),
-    file: 'guilds.json',
-    writer: writeIterableToWriteStream,
+    data: () => from(client.guilds.values()),
+    file: 'guilds.md',
   },
   {
-    data: () => client,
-    file: 'client.txt',
-    writer: writeObjectToWriteStream,
+    data: () => of(client),
+    file: 'client.md',
   },
   {
-    data: () => client
-      .guilds.find((guild) => guild.name === 'roll20bottest')
-      .channels.find((channel) => channel.name === 'general'),
-    file: 'test.txt',
-    writer: writeObjectToWriteStream,
-  }
+    data: () => {
+      const textChannel = client.guilds
+        .find(guild => guild.name === 'roll20bottest')
+        .channels.find(
+          channel => channel.name === 'general' && channel.type === 'text'
+        ) as TextChannel
+      return from(
+        textChannel.fetchMessages({ limit: 10 }).then(messages => {
+          return messages.values()
+        })
+      )
+    },
+    file: 'messages.md',
+  },
 ]
 
-function writeIterableToWriteStream(
-  writeStream: WriteStream,
-  iterable: IterableIterator<any>,
-) {
-  writeStream.write('{')
-
-  let data = iterable.next()
-  while (!data.done) {
-    writeStream.write(
-      `\n"${data.value.id}": ${JSON.stringify(data.value, null, 2)}`,
-    )
-    data = iterable.next()
-    if (!data.done) writeStream.write(',')
-  }
-
-  writeStream.write('}')
+function writeObjectToWriteStream(writeStream: WriteStream, obj: object) {
+  writeStream.write(
+    '```javascript\n' + inspect(obj, { showHidden: true }) + '\n```\n'
+  )
 }
 
-function writeObjectToWriteStream(writeStream: WriteStream, obj: object) {
-  writeStream.write(inspect(obj, { showHidden: true }))
+function writeTestToWriteStream(writeStream: WriteStream, test: Test) {
+  console.log(chalk.greenBright(`Writing ${test.file}`))
+  return test.data().subscribe(value => {
+    writeObjectToWriteStream(writeStream, value as object)
+  })
 }
 
 const dataDir = path.join(__dirname, 'data')
@@ -68,20 +64,32 @@ client
   .login(process.env.TOKEN)
   .then(() => {
     mkdir(dataDir, () => {
-      testDataFiles.forEach(test => {
-        const writeStream = createWriteStream(path.join(dataDir, test.file), { flags: 'w' })
-        test.writer(writeStream, test.data())
-        writeStream.end()
+      const tests = testDataFiles.map(test => {
+        const writeStream = createWriteStream(path.join(dataDir, test.file), {
+          flags: 'w',
+        })
+        return of(writeTestToWriteStream(writeStream, test))
       })
 
-      client.destroy().then(() => {
-        console.log(chalk.greenBright('Your test data feels refreshed.'))
+      forkJoin(tests).subscribe({
+        complete: () => {
+          client.destroy().then(() => {
+            console.log(chalk.greenBright('Your test data feels refreshed.'))
+          })
+        },
+        error: err => {
+          logError(err)
+        },
       })
     })
   })
   .catch(err => {
-    console.log(
-      chalk.redBright(err),
-      chalk.red('\nSomething went wrong. Test data is not refreshed.'),
-    )
+    logError(err)
   })
+
+function logError(err) {
+  console.log(
+    chalk.redBright(err),
+    chalk.red('\nSomething went wrong. Test data is not refreshed.')
+  )
+}
